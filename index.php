207a70bf81fbf71e6a93c2e94bd6b5ffe16df9e3
Habibur Rahman <habiburg3@gmail.com>
2021-09-27 12:31:26 +0600

    blog

--- index.php
+++ index.php
@@ -8,9 +8,11 @@
                     <div class="col-md-9">
 						<?php if ( have_posts() ):while ( have_posts() ):the_post(); ?>
                             <div <?php post_class( 'blog-post  wow fadeInUp' ); ?>>
-                                <a href="blog-details.html"><img class="img-responsive"
-                                                                 src="<?php echo get_template_directory_uri() ?>/assets/images/blog-post/blog_big_01.jpg"
-                                                                 alt=""></a>
+                                <a href="<?php the_permalink(); ?>">
+									<?php if ( has_post_thumbnail() ) {
+										the_post_thumbnail( 'full', array( 'class' => 'img-responsive' ) );
+									} ?>
+                                </a>
                                 <h1><a href="<?php the_permalink(); ?>"><?php the_title() ?></a>
                                 </h1>
                                 <span class="author"><?php the_author() ?></span>
@@ -30,98 +32,20 @@
                             <div class="text-right">
                                 <div class="pagination-container">
                                     <ul class="list-inline list-unstyled">
-                                        <li class="prev"><a href="#"><i class="fa fa-angle-left"></i></a></li>
-                                        <li><a href="#">1</a></li>
-                                        <li class="active"><a href="#">2</a></li>
-                                        <li><a href="#">3</a></li>
-                                        <li><a href="#">4</a></li>
-                                        <li class="next"><a href="#"><i class="fa fa-angle-right"></i></a></li>
+										<?php the_posts_pagination( array(
+											'screen_reader_text' => ' ',
+											'mid_size'           => 4,
+											'prev_text'          => '<i class="fa fa-angle-left"></i>',
+											'next_text'          => '<i class="fa fa-angle-right"></i>',
+										) ); ?>
                                     </ul><!-- /.list-inline -->
+
                                 </div><!-- /.pagination-container -->    </div><!-- /.text-right -->
 
                         </div><!-- /.filters-container -->                </div>
 					<?php get_sidebar() ?>
                 </div>
             </div>
-            <!-- ============================================== BRANDS CAROUSEL ============================================== -->
-            <div id="brands-carousel" class="logo-slider wow fadeInUp">
-
-                <div class="logo-slider-inner">
-                    <div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
-                        <div class="item m-t-15">
-                            <a href="#" class="image">
-                                <img data-echo="<?php echo get_template_directory_uri() ?>/assets/images/brands/brand1.png"
-                                     src="<?php echo get_template_directory_uri() ?>/assets/images/blank.gif" alt="">
-                            </a>
-                        </div><!--/.item-->
-
-                        <div class="item m-t-10">
-                            <a href="#" class="image">
-                                <img data-echo="<?php echo get_template_directory_uri() ?>/assets/images/brands/brand2.png"
-                                     src="<?php echo get_template_directory_uri() ?>/assets/images/blank.gif" alt="">
-                            </a>
-                        </div><!--/.item-->
-
-                        <div class="item">
-                            <a href="#" class="image">
-                                <img data-echo="<?php echo get_template_directory_uri() ?>/assets/images/brands/brand3.png"
-                                     src="<?php echo get_template_directory_uri() ?>/assets/images/blank.gif" alt="">
-                            </a>
-                        </div><!--/.item-->
-
-                        <div class="item">
-                            <a href="#" class="image">
-                                <img data-echo="<?php echo get_template_directory_uri() ?>/assets/images/brands/brand4.png"
-                                     src="<?php echo get_template_directory_uri() ?>/assets/images/blank.gif" alt="">
-                            </a>
-                        </div><!--/.item-->
-
-                        <div class="item">
-                            <a href="#" class="image">
-                                <img data-echo="<?php echo get_template_directory_uri() ?>/assets/images/brands/brand5.png"
-                                     src="<?php echo get_template_directory_uri() ?>/assets/images/blank.gif" alt="">
-                            </a>
-                        </div><!--/.item-->
-
-                        <div class="item">
-                            <a href="#" class="image">
-                                <img data-echo="<?php echo get_template_directory_uri() ?>/assets/images/brands/brand6.png"
-                                     src="<?php echo get_template_directory_uri() ?>/assets/images/blank.gif" alt="">
-                            </a>
-                        </div><!--/.item-->
-
-                        <div class="item">
-                            <a href="#" class="image">
-                                <img data-echo="<?php echo get_template_directory_uri() ?>/assets/images/brands/brand2.png"
-                                     src="<?php echo get_template_directory_uri() ?>/assets/images/blank.gif" alt="">
-                            </a>
-                        </div><!--/.item-->
-
-                        <div class="item">
-                            <a href="#" class="image">
-                                <img data-echo="<?php echo get_template_directory_uri() ?>/assets/images/brands/brand4.png"
-                                     src="<?php echo get_template_directory_uri() ?>/assets/images/blank.gif" alt="">
-                            </a>
-                        </div><!--/.item-->
-
-                        <div class="item">
-                            <a href="#" class="image">
-                                <img data-echo="<?php echo get_template_directory_uri() ?>/assets/images/brands/brand1.png"
-                                     src="<?php echo get_template_directory_uri() ?>/assets/images/blank.gif" alt="">
-                            </a>
-                        </div><!--/.item-->
-
-                        <div class="item">
-                            <a href="#" class="image">
-                                <img data-echo="<?php echo get_template_directory_uri() ?>/assets/images/brands/brand5.png"
-                                     src="<?php echo get_template_directory_uri() ?>/assets/images/blank.gif" alt="">
-                            </a>
-                        </div><!--/.item-->
-                    </div><!-- /.owl-carousel #logo-slider -->
-                </div><!-- /.logo-slider-inner -->
-
-            </div><!-- /.logo-slider -->
-            <!-- ============================================== BRANDS CAROUSEL : END ============================================== -->
         </div>
     </div>
 <?php get_footer() ?>
